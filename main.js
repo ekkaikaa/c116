89d05d4203e0b7244049297af719624cdec5bf6a
ekkaikaa <76554662+ekkaikaa@users.noreply.github.com>
2021-07-27 16:31:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+
+noseX = 0
+noseY = 0
+
+function preload() {}
+
+function setup() {
+    canvas= createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    video.size(300,300);
+    poseNet = ml5.poseNet(video,modelloaded);
+    poseNet.on('pose',gotposes);
+}
+
+function draw() {
+    image(video,0,0,300,300);
+    fill(255,0,0);
+    stroke(0,0,0);
+    circle(noseX, noseY, 20);
+}
+
+function take_snapshot(){
+    save("myfilterimage.png");
+}
+
+function modelloaded() {
+    console.log("poseNet has been initialized")
+}
+
+function gotposes(results) {
+    if(results.length > 0) {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x =" + noseX)
+        console.log("nose y =" + noseY)
+    }
+}
